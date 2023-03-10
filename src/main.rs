497ac83a20ebbda460e7b0ac419ceb4fe0982eb5
unknown <111852433+uneknown@users.noreply.github.com>
2023-03-10 12:50:18 +0100

    Add source code from Fiddle

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
-use fastly::http::StatusCode;
+use fastly::error::anyhow;
+use fastly::geo::geo_lookup;
 use fastly::{Error, Request, Response};
+use serde_json::json;
 
 #[fastly::main]
-fn main(_req: Request) -> Result<Response, Error> {
-    Ok(Response::from_status(StatusCode::OK))
+fn main(req: Request) -> Result<Response, Error> {
+    let client_ip = req
+        .get_client_ip_addr()
+        .ok_or_else(|| anyhow!("could not get client ip"))?;
+    let geo = geo_lookup(client_ip).ok_or_else(|| anyhow!("no geographic data available"))?;
+    let body = json!({
+        "as": {
+            "name": geo.as_name(),
+        },
+        "geo" : {
+            "city": geo.city(),
+            "client_ip": client_ip,
+            "country_name": geo.country_name(),
+            "gmt_offset:": geo.utc_offset().unwrap().to_string(),
+        },
+    });
+
+    Ok(Response::new().with_body_json(&body)?)
 }
